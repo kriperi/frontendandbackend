51e97d826b88daf8f3dc07ec23ea021e925bb2f8
MikkelS <mikkelfs@gmail.com>
2022-04-29 16:49:17 +0200

    Ready for heroku

--- server/server.js
+++ server/server.js
@@ -2,6 +2,13 @@ import express from "express";
 
 const app = express();
 
+app.get("/api/login", (req, res) => {
+  res.json({
+    username: "admin",
+    fullName: "Noen Andre Persson",
+  });
+});
+
 const server = app.listen(process.env.PORT || 3000, () => {
   console.log(`server started in http://localhost:${server.address().port}`);
 });
