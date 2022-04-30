73e44ebb2018bbe4e42ea0fda40dc26b855b6a2c
MikkelS <mikkelfs@gmail.com>
2022-04-30 12:14:42 +0200

    Server react from Express

--- client/index.jsx
+++ client/index.jsx
@@ -1,4 +1,37 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM, { createRoot } from "react-dom/client";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 
-ReactDOM.render(<h1>Hello world</h1>, document.getElementById("app"));
+function FrontPage() {
+  return (
+    <div>
+      <h1>Movie Application</h1>
+
+      <div>
+        <Link to={"/login"}>Login </Link>
+      </div>
+      <div>
+        <Link to={"/register"}>register new user</Link>
+      </div>
+    </div>
+  );
+}
+
+function Login() {
+  return <h1>Hello this is login</h1>;
+}
+
+function Application() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<FrontPage />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+const container = document.getElementById("app");
+const root = createRoot(container);
+root.render(<Application />);
